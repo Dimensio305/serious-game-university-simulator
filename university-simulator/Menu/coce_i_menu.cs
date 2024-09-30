97055cbe836906f347f491901a6fff97c75ca5bc
oceane <bertinoceane57@gmail.com>
2024-09-30 21:38:17 +0200

    ajout des fond du meunu

--- university-simulator/Menu/coce_i_menu.cs
+++ university-simulator/Menu/coce_i_menu.cs
@@ -3,6 +3,20 @@ using System;
 
 public partial class coce_i_menu : Control
 {
+	// variable qui serve pour changer image music
+	private Button button;
+	private Sprite2D sprite;
+	bool etat_son = true;
+	
+	
+	public override void _Ready()
+	{
+	
+		// Récupérer le bouton contenant le Sprite2D
+		button = GetNode<Button>("b_son"); // Chemin du bouton dans ta scène
+		sprite = button.GetNode<Sprite2D>("Music");
+	}
+	
 	
 	public void _on_b_court_pressed(){
 		GetTree().ChangeSceneToFile("res://Jeu_court/jeu_court.tscn");
@@ -17,5 +31,31 @@ public partial class coce_i_menu : Control
 		GetTree().Quit();
 	}
 	
+public void _on_b_son_pressed()
+{
+	GD.Print("Button pressed");  // Vérification que la fonction est appelée
+
+	if (etat_son)
+	{
+		etat_son = false;
+		GD.Print("Turning sound off");
+		
+		var newTexture = (Texture2D)GD.Load("res://asset/icons/PNG/White/1x/musicOff.png");
+
+			sprite.Texture = newTexture;
+		
+	}
+	else
+	{
+		etat_son = true;
+		GD.Print("Turning sound on");
+
+		var newTexture = (Texture2D)GD.Load("res://asset/icons/PNG/White/1x/musicOn.png");
+
+		sprite.Texture = newTexture;
+		
+		
+	}
+}
 	
 }
