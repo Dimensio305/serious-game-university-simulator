10bb03d8b39c01651fd537b07818888644e57c1a
oceane <bertinoceane57@gmail.com>
2024-10-04 20:19:02 +0200

    debut squelette code
    Please enter the commit message for your changes. Lines starting

---
+++ university-simulator/Jeu_court/GestionDb.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public static class GestionDb : Node
+{
+	
+	// ce connecte a la base de donnee envoie un message si bien connecter ou pas 
+
+	public static void connection(){}
+	
+	// met la commande en parametre pour l'exe dans la db
+	public static void executer(string commande){}
+	
+	//supprimer la db
+	public static void supprimer(){}
+	
+	
+}
