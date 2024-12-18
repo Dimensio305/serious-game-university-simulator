660ea78515ead73cb3d3ba1b61d3fdba0a8817aa
oceane <bertinoceane57@gmail.com>
2024-12-18 11:16:49 +0100

    avancement du tuto

--- university-simulator/scenes/Tuto.cs
+++ university-simulator/scenes/Tuto.cs
@@ -14,6 +14,20 @@ public partial class Tuto : Node2D
 	private RichTextLabel labelreponsegauche;
 	private RichTextLabel labelreponsedroite;
 	private RichTextLabel labeldirective;
+
+	bool decouvrequestion = false;
+	bool decouvreetat = false;
+	bool decouvreagenda = false;
+	bool decouvrereponsegauche = false;
+	bool decouvrereponsedroite = false;
+
+	bool etape1 = false;
+	bool etape2 = false;
+	bool etape3 = false;
+	bool etape4 = false;
+	bool inQuestion  = false; 
+
+
 	
 	public override void _Ready()
 	{
@@ -29,10 +43,61 @@ public partial class Tuto : Node2D
 		labelreponsedroite=GetNodeOrNull<RichTextLabel>("Imagerepdroite/reponsedroite");
 		labeldirective=GetNodeOrNull<RichTextLabel>("imagedirective/textlabedirective");
 
+
+
 	}
 
 	
 	public override void _Process(double delta)
 	{
+		
+		if (inQuestion)
+		{
+			
+			
+		}
+
+		if (Input.IsActionJustPressed("agenda"))
+		{
+			
+		}
+
+		if (Input.IsActionJustPressed("formation"))
+		{
+			
+		}
+		if (Input.IsActionJustPressed("Etat"))
+		{
+			
+		}
+
+		if (Input.IsActionPressed("fermer"))
+		{
+		
+		}
 	}
+
+	private async void messadedepart(){
+
+		string message = "Coucou binvenue dans university simulator";
+		fonddirective.Visible=true;
+		labeldirective.Text = $"[center][b][color = black]{message}[/color][/b][/center]";
+		await ToSignal(GetTree().CreateTimer(0.5),"timeout");
+		message += "\n La cle de la reussite est de conserver un equilibre entre tes differentes ressource";
+		labeldirective.Text = $"[center][b][color = black]{message}[/color][/b][/center]";
+		await ToSignal(GetTree().CreateTimer(1),"timeout");
+		message = "Commencons par un tuto rapide pour comprendre les touches";
+		labeldirective.Text = $"[center][b][color = black]{message}[/color][/b][/center]";
+		etape1=true;
+	}
+
+	private void afficherquestion(){
+		if (etape1){
+			fonddirective.Visible=false;
+			string message = " Ici vont s'afficher les diffrente question des composante que vous aller rencontrer"; 
+			labeldirective.Text = $"[center][b][color = black]{message}[/color][/b][/center];
+		}
+	}
+
+
 }
